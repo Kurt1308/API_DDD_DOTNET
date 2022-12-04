2aa7d3d23b939f7ce265cb5f238b063b6c3fa22e
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2022-12-03 22:06:14 -0300

    Implementação Login service

---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services.User;
+using Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+            var baseUser = new UserEntity();
+            if(user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                baseUser = await _repository.FindByLogin(user.Email);
+                if(baseUser == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return baseUser;
+                }
+            } else
+            {
+                return null;
+            }
+        }
+    }
+}
